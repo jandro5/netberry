90418658de19658aa2f32e8dd99559a7ac9de821
alejandro carrera <alexparalosamigos@gmail.com>
2022-09-16 21:40:00 +0200

    Añadido libreria jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,9 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
